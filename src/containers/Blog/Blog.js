ad7bb8ace8ddea2eecfce34ac18934e3148007b7
MARIA ORMAZA <mormaza@thoughtworks.com>
2020-04-09 20:31:29 -0400

    Using react Route to render Posts as / and NewPost as /new-post; with reloading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 import './Blog.css';
 
@@ -20,7 +22,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
